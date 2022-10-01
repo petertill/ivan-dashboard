198103336db547701691b59e454089eacb420067
Peter Till <>
2022-10-01 09:32:54 +0000

    Initial commit

---
+++ database.js
@@ -0,0 +1,104 @@
+
+//MONGODB
+const {MongoClient} = require('mongodb');
+
+
+async function main(){
+    
+    const uri = "mongodb+srv://petyadev:Faszom200@cluster0.5oqtz.mongodb.net/?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    /*  await createListing(client,
+        {
+            name: "Lovely Loft",
+            summary: "A charming loft in Paris",
+            bedrooms: 1,
+            bathrooms: 1
+        }
+    ); */
+
+      //await findOneListingByName(client, "Lovely Loft");
+
+      //await updateListingByName(client, "Infinite Views", { bedrooms: 6, beds: 8 });
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+
+
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+
+
+async function createListing(client, newListing){
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+
+async function findOneListingByName(client, nameOfListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
+
+    if (result) {
+        console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+        console.log(result);
+    } else {
+        console.log(`No listings found with the name '${nameOfListing}'`);
+    }
+}
+
+
+async function updateListingByName(client, nameOfListing, updatedListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .updateOne({ name: nameOfListing }, { $set: updatedListing });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+
+async function upsertListingByName(client, nameOfListing, updatedListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .updateOne({ name: nameOfListing }, 
+                                   { $set: updatedListing }, 
+                                   { upsert: true });
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+
+    if (result.upsertedCount > 0) {
+        console.log(`One document was inserted with the id ${result.upsertedId._id}`);
+    } else {
+        console.log(`${result.modifiedCount} document(s) was/were updated.`);
+    }
+}
+
+
+async function updateAllListingsToHavePropertyType(client) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+                        .updateMany({ property_type: { $exists: false } }, 
+                                    { $set: { property_type: "Unknown" } });
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+                                                            
+
---
+++ index.js
@@ -0,0 +1,176 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+
+const axios = require("axios")
+const process = require("process")
+
+const {MongoClient} = require('mongodb');
+
+
+
+app.use(bodyParser.text());
+
+const uri = "mongodb+srv://petyadev:Faszom200@cluster0.5oqtz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+app.get('/dashboard', function (req, res) {
+  res.sendFile( __dirname + "/" + "generic.html" );
+})
+app.get('/elements', function (req, res) {
+  res.sendFile( __dirname + "/" + "elements.html" );
+})
+app.get('/whats-new', function (req, res) {
+  res.sendFile( __dirname + "/" + "release.html")
+})
+app.post('/', function (req, res) {
+    const data = new FormData();
+    console.log(req.body)
+    data.append('client_id', process.env.CLIENT_ID);
+    data.append('client_secret', process.env.CLIENT_SECRET);
+    data.append('grant_type', 'authorization_code');
+    data.append('redirect_uri', "https://dashh.petertill1.repl.co/dashboard");
+    data.append('scope', 'identify');
+    data.append('code', req.body);
+
+    fetch('https://discordapp.com/api/oauth2/token', {
+        method: 'POST',
+        body: data,
+    })
+        .then(response => response.json())
+        .then(data=>{
+            console.log(data)
+            const config = {
+                headers:{
+                    "authorization":`Bearer ${data.access_token}`
+                }
+            }
+            axios
+                .get("https://discordapp.com/api/users/@me",config)
+                .then(response=>{
+
+                  var stored = (async () => await findDataEntry(parseInt(response.data.id)))();
+                  console.log(stored)
+
+                  stored.then(function(result) {
+   // "Some User token"
+
+              
+                    const obj = {username: response.data.username, discriminator: response.data.discriminator, avatar: response.data.avatar, id: response.data.id, rubels: result.smackers};
+                    const string = JSON.stringify(obj);
+                    console.log(response.data.username)
+                    res.send(string)
+})
+                  
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+        })
+})
+app.listen(8081)
+
+
+
+
+//main().catch(console.error);
+
+
+/*const {MongoClient} = require('mongodb');
+
+
+async function main(){
+    
+    const uri = "mongodb+srv://petyadev:Faszom200@cluster0.5oqtz.mongodb.net/?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        //await  listDatabases(client);
+
+      var array = await findDataEntry(client, 707550269600169984)
+      console.log(array.smackers)
+
+     await createListing(client,
+        {
+            name: "Lovely Loft",
+            summary: "A charming loft in Paris",
+            bedrooms: 1,
+            bathrooms: 1
+        }
+    ); 
+
+      //await findOneListingByName(client, "Lovely Loft");
+
+      //await updateListingByName(client, "Infinite Views", { bedrooms: 6, beds: 8 });
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}*/
+/* async function main(){
+  try {
+
+
+                        //MONGO
+const uri = "mongodb+srv://petyadev:Faszom200@cluster0.5oqtz.mongodb.net/?retryWrites=true&w=majority";
+ 
+
+const client = new MongoClient(uri);
+//Mongo end
+                        // Connect to the MongoDB cluster
+                        client.connect();
+                      } catch (e) {
+                        console.error(e);
+                      } finally {
+                        //client.close();
+                      }
+}
+main().catch(console.error); */
+
+async function findDataEntry(nameOfListing) {
+
+
+  try {
+
+
+                        //MONGO
+const uri = "mongodb+srv://petyadev:Faszom200@cluster0.5oqtz.mongodb.net/?retryWrites=true&w=majority";
+ 
+
+const client = new MongoClient(uri);
+//Mongo end
+                        // Connect to the MongoDB cluster
+                        client.connect();
+                      } catch (e) {
+                        console.error(e);
+                      } finally {
+                        //client.close();
+                      }
+
+
+  
+    const result =  await client.db("coinsdb").collection("coinscollection").findOne({userid: nameOfListing});
+
+    if (result) {
+        console.log(`Found a listing in the collection with the id '${nameOfListing}':`);
+      return result;
+      //console.log(result.smackers)
+    } else {
+        console.log(`No listings found with the id '${nameOfListing}'`);
+    }
+}
